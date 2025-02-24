17f87624ac3972498d7690a26d45ad5e64fc4108
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-24 11:54:07 +0100

    testBlockedState failed

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -9,24 +9,38 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SmartDoorLockTest {
 
     private static final int PIN = 1234;
+    private static final int WRONG_PIN = 4321;
+    private static final int WRONG_ATTEMPS = 4;
 
     private SmartDoorLock smartDoorLock;
 
     @BeforeEach
     public void setup(){
-        smartDoorLock = new SmartDoorLockImpl(PIN);
+        this.smartDoorLock = new SmartDoorLockImpl(PIN);
     }
 
     @Test
     public void testLock() {
         this.smartDoorLock.lock();
-        assertTrue(smartDoorLock.isLocked());
+        assertTrue(this.smartDoorLock.isLocked());
     }
 
     @Test
     public void testUnlock(){
         this.smartDoorLock.unlock(PIN);
-        assertFalse(smartDoorLock.isLocked());
+        assertFalse(this.smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testBlockedState(){
+        this.blockDoor();
+        assertTrue(this.smartDoorLock.isBlocked());
+    }
+
+    private void blockDoor(){
+        for (int i = 0; i < WRONG_ATTEMPS; i++) {
+            this.smartDoorLock.unlock(WRONG_PIN);
+        }
     }
 
 }
