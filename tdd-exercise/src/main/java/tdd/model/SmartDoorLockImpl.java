e4112d20fbbd11a07dc0b4dc284e8c3ab8f9340d
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-24 14:05:16 +0100

    Exercise 2 SmartDoorLock final refactor

--- tdd-exercise/src/main/java/tdd/model/SmartDoorLockImpl.java
+++ tdd-exercise/src/main/java/tdd/model/SmartDoorLockImpl.java
@@ -12,6 +12,7 @@ public class SmartDoorLockImpl implements SmartDoorLock {
     private static final int MAX_ATTEMPTS = 3;
     private static final int DEFAULT_PIN = 0;
     private static final int INITIAL_ATTEMPTS = 0;
+    private static final int PIN_LENGTH = 4;
 
     private int pin;
     private Status status;
@@ -24,6 +25,10 @@ public class SmartDoorLockImpl implements SmartDoorLock {
     @Override
     public void setPin(int pin) {
         if(!this.isBlocked() && !this.isLocked()){
+            String pinToString = "" + pin;
+            if(pinToString.length() != PIN_LENGTH){
+                throw new IllegalArgumentException("Pin must be 4-digit number");
+            }
             this.pin = pin;
         } else {
             throw new IllegalStateException("System is not open");
