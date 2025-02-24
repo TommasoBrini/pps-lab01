407745939ca3468a4fbe9ffa1d862cf0b46f4849
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-24 11:33:44 +0100

    implement test's setup

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,20 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    @BeforeEach
+    public void setup(){
+        SmartDoorLock smartDoorLock = new SmartDoorLockImpl(int pin);
+    }
+
     @Test
     public void todo() {
         assertTrue(true);
     }
+
 }
