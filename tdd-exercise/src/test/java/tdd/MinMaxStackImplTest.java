a2660afee2e43835b97be59113b9e4b49621ddfd
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-24 18:06:13 +0100

    Exercise 2 MinMaxStackImpl: testEmpty failed

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,28 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+
+    MinMaxStack minMaxStack;
+
+    @BeforeEach
+    public void setup(){
+        minMaxStack = new MinMaxStackImpl();
+        this.populate();
+    }
+
+    private void populate(){
+        minMaxStack.push(1);
+        minMaxStack.push(2);
+        minMaxStack.push(3);
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testEmpty() {
+        assertFalse(minMaxStack.isEmpty());
     }
 }
