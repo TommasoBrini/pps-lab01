85559f771a2525e32fd3e08325508cd29a53e3bf
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-24 11:01:07 +0100

    refactor magic number in main

--- basic-refactoring-exercise/src/main/java/example/Main.java
+++ basic-refactoring-exercise/src/main/java/example/Main.java
@@ -6,14 +6,21 @@ import example.model.SimpleBankAccount;
 
 public class Main {
 
+    private static final int FIRST_HOLDER_ID = 1;
+    private static final int INITIAL_BALANCE = 0;
+    private static final int FIRST_DEPOSIT = 100;
+    private static final int FIRST_WITHDRAW = 30;
+    private static final int SECOND_WITHDRAW = 80;
+
+
     public static void main(String[] args) {
-        final AccountHolder accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        final BankAccount bankAccount = new SimpleBankAccount(accountHolder, 0);
-        bankAccount.deposit(accountHolder.getId(), 100);
+        final AccountHolder accountHolder = new AccountHolder("Mario", "Rossi", FIRST_HOLDER_ID);
+        final BankAccount bankAccount = new SimpleBankAccount(accountHolder, INITIAL_BALANCE);
+        bankAccount.deposit(accountHolder.getId(), FIRST_DEPOSIT);
         System.out.println("Current balance is " + bankAccount.getBalance());
-        bankAccount.withdraw(accountHolder.getId(), 30);
+        bankAccount.withdraw(accountHolder.getId(), FIRST_WITHDRAW);
         System.out.println("Current balance is " + bankAccount.getBalance());
-        bankAccount.withdraw(accountHolder.getId(), 80);
+        bankAccount.withdraw(accountHolder.getId(), SECOND_WITHDRAW);
         System.out.println("Current balance is " + bankAccount.getBalance());
     }
 }
