f0e62cecb1a53be8e376d3aca55481ef7cd3746a
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-25 16:56:49 +0100

    Exercise 2 task3: testPush failed

--- tdd-exercise/src/test/java/tdd/CircularListTest.java
+++ tdd-exercise/src/test/java/tdd/CircularListTest.java
@@ -1,6 +1,7 @@
 package tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,8 +10,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+
+    private static final int SIZE = 3;
+    private CircularQueue circularQueue;
+
+    @BeforeEach
+    public void setup(){
+        circularQueue = new CircularQueueImpl(SIZE);
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testEmpty(){
+        assertTrue(circularQueue.isEmpty());
+    }
+
+
+    @Test
+    public void testPush() {
+        circularQueue.push(1);
+        assertTrue(1, circularQueue.size());
     }
 }
