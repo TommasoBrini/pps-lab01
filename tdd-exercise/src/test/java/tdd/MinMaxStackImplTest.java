60813a1b17ffa83970c10573fc7d54342057fbd5
Tommaso <tommaso.brini@studio.unibo.it>
2025-02-25 15:43:40 +0100

    Exercise 2 task2: testMin failed

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MinMaxStackImplTest {
 
     private static final int INITIAL_SIZE = 5;
+    private static final int MIN_VALUE = 0;
 
     MinMaxStack minMaxStack;
 
@@ -19,7 +20,7 @@ class MinMaxStackImplTest {
     }
 
     private void populate(){
-        for(int i = 0; i < INITIAL_SIZE; i++){
+        for(int i = MIN_VALUE; i < INITIAL_SIZE; i++){
             minMaxStack.push(i);
         }
     }
@@ -39,11 +40,22 @@ class MinMaxStackImplTest {
         assertAll(() -> assertEquals(minMaxStack.peek(), INITIAL_SIZE - 1), () -> assertEquals(minMaxStack.size(), INITIAL_SIZE));
     }
 
-    @Test
-    public void testPopEmptyStack() {
+    private void clearStack() {
         for (int i = 0; i < INITIAL_SIZE; i++){
             minMaxStack.pop();
         }
+    }
+
+    @Test
+    public void testPopEmptyStack() {
+        clearStack();
         assertThrowsExactly(IllegalStateException.class, () -> minMaxStack.pop());
+    }
+
+    @Test
+    public void testMin(){
+        assertEquals(minMaxStack.getMin(), MIN_VALUE);
+        clearStack();
+        assertThrowsExactly(IllegalStateException.class, () -> minMaxStack.getMin());
     }
 }
